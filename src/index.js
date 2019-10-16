04e755ebc64c8932890d76701b47d04d7ce7f2c7
Jae <ujpyon@gmail.com>
2019-10-16 18:07:34 -0400

    Add density map of data

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
-// Domain for the current Summoner's Rift on the in-game mini-map
-domain = {
-  min: { x: -120, y: -120 },
-  max: { x: 14870, y: 14980 }
-},
-width = 512,
-height = 512,
-bg =
-  "https://matchhistory.na.leagueoflegends.com/assets/1.0.36/images/maps/map11.png",
-xScale,
-yScale,
-svg;
+let coords = [],
+  width = 512,
+  height = 512,
+  domain = {
+    min: { x: -120, y: -120 },
+    max: { x: 14870, y: 14980 }
+  },
+  // bg = "https://matchhistory.na.leagueoflegends.com/assets/1.0.36/images/maps/map11.png",
+  bg = "./assets/images/minimap.png",
+  xScale,
+  yScale,
+  svg;
 
 color = d3.scaleLinear()
     .domain([0, 3])
@@ -50,6 +50,38 @@ d3.json("./match_data/iron_match_data.json", function(data) {
     .attr("cy", function(d) {
       return yScale(d.position.y);
     })
-    .attr("r", 5)
-    .attr("class", "kills");
+    .attr("r", 4)
+    .style("fill", function(d) {
+      if (d.victimId < 6) {
+        return "blue";
+      } else {
+        return "red";
+      }
+    })
+
+  let color = d3
+    .scaleLinear()
+    .domain([0, 1])
+    .range(["transparent", "#32CD32"]);
+
+  let densityData = d3.contourDensity()
+    .x(function(d) {
+      return xScale(d.position.x);
+    })
+    .y(function(d) {
+      return yScale(d.position.y);
+    })
+    .size([width, height])
+    .bandwidth(20)(dataPoint);
+
+  svg.insert("g", "g")
+    .selectAll("path")
+    .data(densityData)
+    .enter()
+    .append("path")
+    .attr("d", d3.geoPath())
+    .attr("fill", function(d) {
+      console.log(d.value * 300)
+      return color(d.value * 300);
+    });
 });
