2db66120286b91a62da1ab2f632df9fba2ba3854
Jae <ujpyon@gmail.com>
2019-10-17 19:22:41 -0400

    Make bar graph appear horizontally

--- src/bar_graph_stats.js
+++ src/bar_graph_stats.js
@@ -3,6 +3,16 @@ const barWidth = 200;
 const innerWidth = barWidth - 20;
 const barGap = 8;
 
+
+let changeBars = function(dataPath) {
+  debugger
+  d3.json(dataPath, function(data) {
+    debugger
+    let barData = data.data;
+    updateBars(preprocess(barData));
+  });
+};
+
 const svg3 = d3
   .select("#barGraph")
   .append("svg")
@@ -11,6 +21,7 @@ const svg3 = d3
   .append("g");
 
 function preprocess(givenData) {
+  debugger
   return givenData.map((d, i) => ({ id: i, value: d }));
 }
 
@@ -23,7 +34,7 @@ function y(d) {
   return barWidth - scale(d.value);
 }
 
-function scaleWidth(d) {
+function width(d) {
   return scale(d.value);
 }
 
@@ -31,20 +42,25 @@ function updateBars(data) {
   const t = d3.transition().duration(800);
   const bar = svg3.selectAll("g").data(data, d => d.id);
 
+  debugger
   bar.exit().remove();
 
+  debugger
   bar
     .transition(t)
     .attr(
       "transform",
-      (d, i) => `translate(${i * (barHeight + barGap)},${y(d)})`
+      // (d, i) => `translate(${i * (barHeight + barGap)},${y(d)})`
+      (d, i) => `translate(${i * (barHeight + barGap)},10)`
     );
 
+  debugger
   bar
     .select("rect")
     .transition(t)
     .attr("height", 30);
 
+  debugger
   bar
     .select("text")
     .transition(t)
@@ -55,21 +71,21 @@ function updateBars(data) {
       return t => (this.textContent = i(t));
     });
 
+  debugger
   const barEnter = bar
     .enter()
     .append("g")
     .attr(
       "transform",
-      (d, i) => `translate(${i * (barHeight + barGap)},${innerWidth})`
+      (d, i) => `translate(${i * (30 + barGap)},${innerWidth})`
     );
 
+  debugger
   barEnter
     .transition(t)
-    .attr(
-      "transform",
-      (d, i) => `translate(${i * (barHeight + barGap)},${y(d)})`
-    );
+    .attr("transform", (d, i) => `translate(0,${i * (30 + barGap)})`);
 
+  debugger
   const rect = barEnter
     .append("rect")
     .attr("x", 0)
@@ -77,17 +93,25 @@ function updateBars(data) {
     .attr("height", 30)
     .attr("width", 0);
 
+  debugger
   rect.transition(t).attr("width", barWidth);
 
+  debugger
   barEnter
     .append("text")
-    .text(d => d.value)
-    .attr("text-anchor", "start")
-    .attr("dx", barWidth)
-    .attr("dy", -2);
+    .text(d => {
+      debugger
+      return d.value
+    }
+      )
+    .attr("text-anchor", "middle")
+    .attr("dx", barWidth / 2 )
+    .attr("dy", 20)
+    .style("fill", "white");
 }
 
 d3.json("./match_data/challenger_match_data.json", function(data) {
+  debugger
   let barData = data.data;
   updateBars(preprocess(barData));
 });
